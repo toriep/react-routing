75d560c875b42cd8bbe9e05b85c45996b5460d21
Torie Pham <tientp@uci.edu>
2019-05-27 22:19:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const chooseRandomAnimal = () => {
+  const animals = ['weddell seal', 'red panda', 'snow lynx', 'platypus', 'polar bear']
+  const randomIndex = Math.floor(Math.random() * animals.length);
+  return animals[randomIndex];
+};
 
-export default App;
+const Home = () => (
+  <>
+    <h1>This is the home page</h1>
+  </>
+);
+
+const Food = () => (
+  <>
+    <h1>Seafood and soups are my favorites</h1>
+  </>
+);
+
+const Animal = () => (
+  <>
+    <h1>My favorite animal is the {chooseRandomAnimal()}</h1>
+  </>
+);
+
+const NoMatchPage = () => (
+  <>
+    <h1>404 Not Found. Page does not exist.</h1>
+  </>
+);
+
+const App = () => {
+  return (
+    <>
+      <ul id='navBar'>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/food'>Food</Link></li>
+        <li><Link to='/animal/:animal'>Animal</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/food' component={Food} />
+        <Route path='/animal/:animal' component={Animal} />
+        <Route component={NoMatchPage}/>
+      </Switch>
+    </>
+  );
+};
+
+export default App; 
